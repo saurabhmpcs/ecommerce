63d19722d3de6c3bb26139b65c3896b71ec0ccad
Saurabh Singh <saurabh.rgt@gmail.com>
2023-07-12 16:58:45 +0530

    12th-july

---
+++ components/Product.jsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import React from "react";
+
+import { urlFor } from "../lib/client";
+
+const Product = ({ product: { image, name, slug, price } }) => {
+  return (
+    <div>
+      <Link href={`/product/${slug.current}`}>
+        <div className="product-card">
+          <img
+            src={urlFor(image && image[0])}
+            width={250}
+            height={250}
+            className="product-image"
+          />
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default Product;
---
+++ ecommerce/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'vofbym28',
+    dataset: 'production'
+  }
+})
