e023fcbc8688426c96fdcf65a72782eba4a62601
Saurabh Singh <saurabh.rgt@gmail.com>
2023-07-17 21:09:16 +0530

    stripe-integration

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
-const Layout = () => {
-  return <div>Layout</div>;
+import Head from "next/head";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+const Layout = ({ children }) => {
+  return (
+    <div className="layout">
+      <Head>
+        <title>Saurabh Mastery Store</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className="main-container">{children}</main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 };
 
 export default Layout;
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,18 @@
+import { Layout } from "../components";
 import "../styles/globals.css";
 import React from "react";
 
+import { Toaster } from "react-hot-toast";
+
+import { StateContext } from "../context/StateContext";
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
+  );
 }
